75cb2f2bcf47b4dde29915ddd45072e84b666dd7
John Willard <willard.john.gray@gmail.com>
2025-03-17 23:36:41 -0400

    add skeleton of emulator

--- chip8.c
+++ chip8.c
@@ -1,6 +1,65 @@
 #include "chip8.h"
+#include "SDL_timer.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void chip8_init(Chip8 *chip) { memset(chip, 0, sizeof(Chip8)); }
+void chip8_init(Chip8 *chip) {
+  memset(chip, 0, sizeof(Chip8));
+  chip->PC = PROGRAM_START;
+}
+
+void chip8_load_rom(Chip8 *chip, const char *filename) {
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    perror("Failed to open ROM");
+    return;
+  }
+
+  fseek(file, 0, SEEK_END);
+  size_t size = ftell(file);
+  rewind(file);
+
+  if (size > (MEMORY_SIZE - PROGRAM_START)) {
+    perror("ROM too large");
+    fclose(file);
+    return;
+  }
+
+  fread(&chip->memory[PROGRAM_START], size, 1, file);
+  fclose(file);
+}
+
+void chip8_run(Chip8 *chip, chip8_draw_callback draw,
+               chip8_input_callback input, void *userdata) {
+  uint64_t last_time = SDL_GetTicks();
+
+  while (true) {
+    uint32_t current_time = SDL_GetTicks();
+    uint32_t elapsed_time = current_time - last_time;
+
+    if (elapsed_time >= (1000 / 60)) {
+      last_time = current_time;
+
+      if (input) {
+        input(chip->keypad);
+      }
+
+      chip8_cycle(chip);
+
+      if (draw) {
+        draw(userdata);
+      }
+    }
+  }
+}
+
+void chip8_cycle(Chip8 *chip) {
+  // fetch 16-bit opcode
+  uint16_t opcode = chip->memory[chip->PC] << 8 | chip->memory[chip->PC + 1];
+
+  // TODO decode
+}
--- chip8.h
+++ chip8.h
@@ -3,13 +3,32 @@
 
 #include <stdint.h>
 
+#define PROGRAM_START 0x200
+
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
+#define MEMORY_SIZE 4096
+#define NUM_REGISTERS 16
+#define STACK_SIZE 16
+#define KEYPAD_SIZE 16
 
 typedef struct Chip8 {
   uint8_t display[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+  uint8_t memory[MEMORY_SIZE];
+  uint16_t I;
+  uint16_t PC;
+  uint16_t stack[STACK_SIZE];
+  uint8_t SP;
+  uint8_t keypad[KEYPAD_SIZE];
 } Chip8;
 
+typedef void (*chip8_draw_callback)(void *userdata);
+typedef void (*chip8_input_callback)(uint8_t *keypad);
+
 void chip8_init(Chip8 *chip);
+void chip8_rom_load(Chip8 *chip, const char *filename);
+void chip8_run(Chip8 *chip, chip8_draw_callback draw,
+               chip8_input_callback input, void *userdata);
+void chip8_cycle(Chip8 *chip);
 
 #endif
